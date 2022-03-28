ba0a27ce93632fadb5ecf20f1bf00f55065054c7
Patryk Wojtiuk <patryk.wojtiuk9@gmail.com>
2022-03-28 11:24:25 +0200

    home page end

---
+++ FoodTime/FoodTime/Controllers/SignupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodTime.Controllers;
+
+public class SignupController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
